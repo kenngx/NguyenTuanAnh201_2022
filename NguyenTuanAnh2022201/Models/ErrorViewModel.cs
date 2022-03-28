62393c69ae261c26f7e15dd44c3b5c7e5b633e46
KenNgx <ngxtuananh99@gmail.com>
2022-03-28 07:15:12 +0700

    First_commit_NTA201_2022

---
+++ NguyenTuanAnh2022201/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenTuanAnh2022201.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
